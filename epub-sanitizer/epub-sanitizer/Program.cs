bef1c882faefe807c8c46b7f42d3b1a1a78b30b6
antobe <antonoberg3@outlook.com>
2023-06-23 00:29:39 +0200

    Cleanup

--- epub-sanitizer/epub-sanitizer/Program.cs
+++ epub-sanitizer/epub-sanitizer/Program.cs
@@ -1,24 +1,32 @@
 ﻿namespace epub_sanitizer;
 
-using System.Text.RegularExpressions;
-using System.Text;
-using VersOne.Epub;
 using HtmlAgilityPack;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using VersOne.Epub;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        EpubBook book = EpubReader.ReadBook(GetFilePath());
-        for (int i = 1; i < book.ReadingOrder.Count; i++)
+        try
         {
-            ReadFile(book.ReadingOrder.ToArray()[i]);
+            var book = EpubReader.ReadBook(GetFilePath());
+            for (int i = 1; i < book.ReadingOrder.Count; i++)
+            {
+                ReadFile(book.ReadingOrder.ToArray()[i]);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
         }
     }
 
     private static string GetFilePath()
     {
-        var location = System.Reflection.Assembly.GetExecutingAssembly().Location;
+        var location = Assembly.GetExecutingAssembly().Location;
         var directoryName = Path.GetDirectoryName(location);
         //var contents = File.ReadAllLines(Directory.EnumerateFiles(directoryName, "*.epub")?.FirstOrDefault());
 
@@ -27,26 +35,27 @@ internal class Program
 
     private static void ReadFile(EpubLocalTextContentFile file)
     {
-        HtmlDocument htmlDocument = new HtmlDocument();
-        htmlDocument.OptionFixNestedTags = true;
+        var htmlDocument = new HtmlDocument
+        {
+            OptionFixNestedTags = true
+        };
         htmlDocument.LoadHtml(file.Content.ToString());
 
-        StringBuilder stringBuilder = new StringBuilder();
+        var stringBuilder = new StringBuilder();
         foreach (HtmlNode node in htmlDocument.DocumentNode.SelectNodes("//text()"))
         {
-
             if (node.InnerText == "Read latest Chapters at Wuxia World . Site Only")
             {
                 node.RemoveAllChildren();
-
             }
         }
 
         var nodes = htmlDocument.DocumentNode.SelectNodes("//style");
-
         foreach (var node in nodes)
+        {
             node.ParentNode.RemoveChild(node);
-        _ = htmlDocument.DocumentNode.OuterHtml;
+            _ = htmlDocument.DocumentNode.OuterHtml;
+        }
 
         Console.WriteLine(stringBuilder.AppendLine(htmlDocument.DocumentNode.OuterHtml.ToString()));
     }
