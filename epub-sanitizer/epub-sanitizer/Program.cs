7b965b04f28ad33cac4c48a466e8f31eb355de60
antobe <antonoberg3@outlook.com>
2024-07-07 20:30:05 +0200

    WIP

--- epub-sanitizer/epub-sanitizer/Program.cs
+++ epub-sanitizer/epub-sanitizer/Program.cs
@@ -1,68 +1,106 @@
 ﻿namespace epub_sanitizer;
 
 using HtmlAgilityPack;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Xml.Linq;
 using VersOne.Epub;
-using VersOne.Epub.Options;
 
 internal class Program
 {
-    // Make program userfriendly (Low prio feature)
-    //  - Input filepath to any location on disc containing books
-    //  - Store filapath location on config file
-    //      - Update filepath only if input differs 
-
-    // Get all files in Books folder and get all book names/titles
-
-    // Create list of filepaths
-
-    // Create a job for each book
-    //  
-    //  Feature? Detection stragety of book source/type with common defects
-    //  - create temp copy of book
-    //  - read each page
-    //  - detect defects
-    //  - sanitize defect
-    //  - save overwrite page
-
-    // Add to paralelles pool
-
-    // Run all jobs simultaneously
-
-    // Done
-
     static void Main(string[] args)
     {
         try
         {
             var sw = new Stopwatch();
+            var xmlParseErrors = new List<KeyValuePair<string, string>>();
             sw.Start();
-            var saveLocationPath = GetSaveFolderPath();
             var files = GetFileNames();
-
-            foreach ( var file in files )
+            foreach (var file in files)
             {
-                if (file == null)
-                {
-                    continue;
-                }
+                if (file == null) continue;
+
                 var book = EpubReader.ReadBook(GetFilePath(file));
-                for (int i = 1; i < book.ReadingOrder.Count; i++)
+                var pageCountTotal = book.ReadingOrder.Count;
+                
+                for (int i = 1; i < pageCountTotal; i++)
                 {
-                    ReadFile(book.ReadingOrder.ToArray()[i]);
+                    var page = book.ReadingOrder.ToArray()[i];
+                    var sanitizedPage = Sanitize(page);
+                    var trimmedKey = sanitizedPage.Key.Replace("Text/", string.Empty);
+
+                    var saveFolderPath = GetSaveFolderPath();
+                    var newFile = $"{saveFolderPath}\\{trimmedKey}";
+                    
+                    try
+                    {
+                        var parsedDoc = XDocument.Parse(sanitizedPage.Content, LoadOptions.PreserveWhitespace);
+                        using var writer = new StreamWriter(newFile);
+                        writer.Write(sanitizedPage.Content);
+
+                    }
+                    catch (Exception ex) 
+                    {
+                        xmlParseErrors.Add(new KeyValuePair<string, string>(trimmedKey, ex.Message));
+                    }
+                    RunLocalProcess(file, newFile);
                 }
             }
             sw.Stop();
             Console.WriteLine($"Elapsed time: {sw.Elapsed.TotalSeconds} sek");
+            foreach (var item in xmlParseErrors)
+            {
+                Console.WriteLine($"Item key:{item.Key} Error: {item.Value}");
+            }
         }
         catch (Exception ex)
         {
             Console.WriteLine(ex.Message);
         }
+    }
+
+    private static EpubLocalTextContentFile Sanitize(EpubLocalTextContentFile file)
+    {
+        var htmlDocument = new HtmlDocument
+        {
+            OptionFixNestedTags = true
+        };
+        htmlDocument.LoadHtml(file.Content.ToString());
+
+        var stringBuilder = new StringBuilder();
+        foreach (HtmlNode node in htmlDocument.DocumentNode.SelectNodes("//text()"))
+        {
+            if (node.InnerText == "Read latest Chapters at Wuxia World . Site Only")
+            {
+                node.RemoveAllChildren();
+            }
+        }
+        var links = htmlDocument.DocumentNode.SelectNodes("//link");
+        if (links != null)
+        {
+            foreach (var link in links)
+            {
+                link.ParentNode.RemoveChild(link);
+                _ = htmlDocument.DocumentNode.OuterHtml;
+            }
+        }
+
+        var nodes = htmlDocument.DocumentNode.SelectNodes("//style");
+        if (nodes != null)
+        {
+            foreach (var node in nodes)
+            {
+                node.ParentNode.RemoveChild(node);
+                _ = htmlDocument.DocumentNode.OuterHtml;
+            }
+        }
+        var sb = stringBuilder.AppendLine(htmlDocument.DocumentNode.OuterHtml.ToString());
+        var sanitizedFile = new EpubLocalTextContentFile(file.Key, file.ContentType, file.ContentMimeType, file.FilePath, sb.ToString());
+        return sanitizedFile;
     }
 
     private static string GetFilePath(string fileName)
@@ -80,7 +118,7 @@ internal class Program
         var directoryName = Path.GetDirectoryName(location);
         Directory.CreateDirectory($"{directoryName}/sanitized");
 
-        return $"{directoryName}/sanitized";
+        return $"{directoryName}\\sanitized";
     }
 
     private static IEnumerable<string?> GetFileNames()
@@ -92,39 +130,31 @@ internal class Program
         return fileNames;
     }
 
-    private static void ReadFile(EpubLocalTextContentFile file)
+    private static void RunLocalProcess(string epubFileName, string newFile)
     {
-        Console.WriteLine(file.Content);
-        var htmlDocument = new HtmlDocument
+        var location = Assembly.GetExecutingAssembly().Location;
+        var directoryName = Path.GetDirectoryName(location);
+        var fileName = $"{directoryName}/scripts/app.py -i {epubFileName} -i {newFile}";
+
+        ProcessStartInfo start = new()
         {
-            OptionFixNestedTags = true
+            FileName = @"C:\Program Files\Python312\python.exe",
+            Arguments = fileName,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+            RedirectStandardOutput = true
         };
-        htmlDocument.LoadHtml(file.Content.ToString());
-
-        var stringBuilder = new StringBuilder();
-        foreach (HtmlNode node in htmlDocument.DocumentNode.SelectNodes("//text()"))
+        string result = "";
+        using (var process = Process.Start(start)!)
         {
-            if (node.InnerText == "Read latest Chapters at Wuxia World . Site Only")
-            {
-                node.RemoveAllChildren();
-            }
+            result = process!.StandardOutput.ReadToEnd();
+            process.Kill();
         }
-
-        var nodes = htmlDocument.DocumentNode.SelectNodes("//style");
-        if (nodes != null)
-        {
-            foreach (var node in nodes)
-            {
-                node.ParentNode.RemoveChild(node);
-                _ = htmlDocument.DocumentNode.OuterHtml;
-            }
-        }
-
-        Console.WriteLine(stringBuilder.AppendLine(htmlDocument.DocumentNode.OuterHtml.ToString()));
+        Console.WriteLine(result);
     }
-
     public static string StripHTML(string input)
     {
+
         return Regex.Replace(input, "<.*?>", String.Empty);
     }
 }
